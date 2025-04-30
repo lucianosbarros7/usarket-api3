27438976542cdff6308f8bd2e2272ef9a782857a
lucianosbarros7 <lucianosbarros@gmail.com>
2025-04-30 13:02:56 -0300

    Update market.js

--- api/market.js
+++ api/market.js
@@ -1,11 +1,15 @@
 export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // ou use o domínio exato em produção
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
   const { index } = req.query;
 
   const mockData = {
-    spx:   [4500, 4510, 4490, 4520, 4550],
+    spx: [4500, 4510, 4490, 4520, 4550],
     nasdaq: [14800, 14820, 14750, 14900, 15000],
-    dow:   [40400, 40550, 40600, 40750, 40800],
-    msci:  [1700, 1710, 1715, 1720, 1725],
+    dow: [40400, 40550, 40600, 40750, 40800],
+    msci: [1700, 1710, 1715, 1720, 1725],
     russell: [1880, 1885, 1890, 1900, 1910]
   };
 
